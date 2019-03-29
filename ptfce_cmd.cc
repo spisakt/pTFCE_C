7db1fa4ec8993f60200090b9a2351f1fbbba3377
Tamas Spisak <spisak.tms@gmail.com>
2019-03-29 15:26:33 +0100

    comments added

--- ptfce_cmd.cc
+++ ptfce_cmd.cc
@@ -13,17 +13,20 @@
 
 int main(int argc, char* argv[])
 {
+    // Load data
     NEWIMAGE::volume4D<float> R;
     NEWIMAGE::volume<float> mask;
     read_volume(R,    argv[1]);
     read_volume(mask, argv[2]);
 
-
+    // perform pTFCE
     pTFCE<float> enhance(R, mask);
     enhance.printSmoothness();
-    enhance.setThresholdCount(12);
+    enhance.setThresholdCount(100); //deafult in R package
     enhance.calculate();
-    save_volume(enhance.pTFCEimg, "testdata/pTFCE.nii.gz");
-    save_volume(enhance.logp_pTFCE, "testdata/logpTFCE.nii.gz");
-    save_volume(enhance.Z_pTFCE, "testdata/ZpTFCE.nii.gz");
+
+    // save enhanced volume
+    save_volume(enhance.pTFCEimg, "mytest/pTFCE.nii.gz");
+    save_volume(enhance.logp_pTFCE, "mytest/logpTFCE.nii.gz");
+    save_volume(enhance.Z_pTFCE, "mytest/ZpTFCE.nii.gz");
 }
